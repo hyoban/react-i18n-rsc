ba1bb2930595b13642d302a532cc905c29f59a7c
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-05 11:56:36 +0800

    chore: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,5 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   react: true,
   pnpm: false,
+  ignores: ['src/pages.gen.ts'],
 })
--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -1,21 +1,21 @@
 // deno-fmt-ignore-file
 // biome-ignore format: generated types do not need formatting
 // prettier-ignore
-import type { GetConfigResponse, PathsForPages } from 'waku/router'
+import type { PathsForPages, GetConfigResponse } from 'waku/router';
 
 // prettier-ignore
-import type { getConfig as File_Index_getConfig } from './pages/index'
+import type { getConfig as File_Index_getConfig } from './pages/index';
 
 // prettier-ignore
-type Page
-  = | ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>)
+type Page =
+| ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>);
 
 // prettier-ignore
 declare module 'waku/router' {
   interface RouteConfig {
-    paths: PathsForPages<Page>
+    paths: PathsForPages<Page>;
   }
   interface CreatePagesConfig {
-    pages: Page
+    pages: Page;
   }
 }
