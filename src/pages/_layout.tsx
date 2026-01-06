778bfb46f5b2615f22c90ad5b88512d47f5f5b0d
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-06 20:35:19 +0800

    chore: remove dark mode

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -9,15 +9,6 @@ const globalStyles = `
     --background: #ffffff;
     --foreground: #171717;
   }
-  @media (prefers-color-scheme: dark) {
-    :root {
-      --background: #0a0a0a;
-      --foreground: #ededed;
-    }
-    html {
-      color-scheme: dark;
-    }
-  }
   html, body {
     max-width: 100vw;
     overflow-x: hidden;
